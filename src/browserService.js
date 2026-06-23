77bb939b26e63b705c245ab4b80c1a2a881cbd37
wuuconix <wuuconix@qq.com>
2026-06-23 09:15:09 +0800

    Revert "调整浏览器宽度，拉大"
    This reverts commit ae7691e9a3e087624b0b853a96b07f0d6dc23c9c.

--- src/browserService.js
+++ src/browserService.js
@@ -90,7 +90,7 @@ class BrowserService {
             await targetPage.bringToFront();
         }
         this.page = targetPage;
-        await targetPage.setViewport({ width: 1880, height: 900 });
+        await targetPage.setViewport({ width: 1280, height: 900 });
         await targetPage.setExtraHTTPHeaders({
             'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
         });
@@ -102,7 +102,7 @@ class BrowserService {
                 get: () => ['zh-CN', 'zh', 'en-US', 'en'],
             });
         });
-        console.log('[Browser] 浏览器已启动 (1880x900)');
+        console.log('[Browser] 浏览器已启动 (1280x900)');
     }
 
     clearUserDataDir() {
